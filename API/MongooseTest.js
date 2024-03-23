09f178514f73171bac2901133c41dbb847f86dd8
Jack123412 <149205237+Jack123412@users.noreply.github.com>
2024-03-23 11:49:22 -0400

    mongoose test updated

---
+++ API/MongooseTest.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Club = require(".\\Models\\Club");
+require('dotenv').config();
+
+
+async function getClubs() {
+    mongoose.connect(process.env.MONGO_API_KEY);
+    const clubs = await Club.find();
+    for (let i = 0; i < clubs.length; i ++) {
+        console.log(clubs[i]);
+    }
+    mongoose.disconnect();
+}
+
+getClubs();
