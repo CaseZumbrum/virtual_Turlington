c06b6fd3c0f2d76d19ca53a8a539ae06b3202007
Jack123412 <149205237+Jack123412@users.noreply.github.com>
2024-03-24 03:34:59 -0400

    the formatting for landing page

--- client/src/help/ListsAndThings.js
+++ client/src/help/ListsAndThings.js
@@ -40,3 +40,93 @@ export const testClub = {
   app: true,
   interactions: 1000,
 };
+
+export const testClub1 = {
+  name: "Hero Club",
+  info: "We just LOVE to BE Heros. Like so much! But NOT to die...",
+  meetStart: "17",
+  PM: true,
+  meetLength: ["180"], //of size 1...
+  day: [0, 2, 3],
+  freq: 3,
+  links: [
+    "https://www.honda.com",
+    "https://www.google.com",
+    "https://www.yahoo.com",
+  ],
+  tags: ["Clouds", "Chimes", "Golden Light"],
+  app: true,
+  interactions: 10000,
+};
+
+export const testClub2 = {
+  name: "Swimming Club",
+  info: "We just LOVE to SWIM. Like so much! But NOT to dog...",
+  meetStart: "10",
+  PM: true,
+  meetLength: ["90"], //of size 1...
+  day: [1, 3, 4],
+  freq: 3,
+  links: [
+    "https://www.honda.com",
+    "https://www.google.com",
+    "https://www.yahoo.com",
+  ],
+  tags: ["Crab", "Lobster", "Shrimp"],
+  app: false,
+  interactions: 1000,
+};
+
+export const testClub3 = {
+  name: "Dog Club",
+  info: "We just LOVE to Dog. Like so much! But NOT to log...",
+  meetStart: "12",
+  PM: true,
+  meetLength: ["60"], //of size 1...
+  day: [0, 4, 5],
+  freq: 1,
+  links: [
+    "https://www.honda.com",
+    "https://www.google.com",
+    "https://www.yahoo.com",
+  ],
+  tags: ["Apple", "Bapple", "Capple"],
+  app: true,
+  interactions: 1000,
+};
+
+export const testClub4 = {
+  name: "Martyr Club",
+  info: "We just LOVE to BE Remembered. Like so much! But NOT to lead...",
+  meetStart: "17",
+  PM: true,
+  meetLength: ["180"], //of size 1...
+  day: [0, 2, 3],
+  freq: 3,
+  links: [
+    "https://www.honda.com",
+    "https://www.google.com",
+    "https://www.yahoo.com",
+  ],
+  tags: ["Fire", "Brimstone", "Gnashing of Teeth"],
+  app: true,
+  interactions: 10000,
+};
+
+export const testClub5 = {
+  name: "Jumping Club",
+  info: "We just LOVE to Jump. Like so much! But NOT to Fall...",
+  meetStart: "10",
+  PM: true,
+  meetLength: ["90"], //of size 1...
+  day: [1, 3, 4],
+  freq: 3,
+  links: [
+    "https://www.honda.com",
+    "https://www.google.com",
+    "https://www.yahoo.com",
+  ],
+  tags: ["Crab", "Lobster", "Shrimp"],
+  app: false,
+  interactions: 1000,
+};
